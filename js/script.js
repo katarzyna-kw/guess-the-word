d30f8829bccc3f735a0c7f1afee902cc9b68626a
katarzyna_w <katarzyna.koch@gmail.com>
2021-06-12 15:33:43 -0500

    created global variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const textInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again hide");
+
+const word = "magnolia";
+
